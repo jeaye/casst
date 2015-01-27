809f75e77a9a22dfdad74122bc8203f130001544
jeaye <contact@jeaye.com>
2015-01-27 17:07:15 +0800

    Formatting changes

--- test/include/delete/delete.hpp
+++ test/include/delete/delete.hpp
@@ -16,89 +16,152 @@ namespace jest
   template <> template <>
   void casst::delete_group::test<0>() /* cols */
   {
-    expect_equal(casst::delete_().from("turb").to_string(),
-                 "DELETE FROM turb ");
-    expect_equal(casst::delete_().cols().from("turb").to_string(),
-                 "DELETE FROM turb ");
-    expect_equal(casst::delete_().cols("col1", "col2", "col3").
-                                  from("turb").to_string(),
-                 "DELETE col1, col2, col3 FROM turb ");
+    expect_equal
+    (
+      casst::delete_().from("turb").to_string(),
+
+      "DELETE FROM turb "
+    );
+    expect_equal
+    (
+      casst::delete_().cols().from("turb").to_string(),
+
+      "DELETE FROM turb "
+    );
+    expect_equal
+    (
+      casst::delete_().cols("col1", "col2", "col3").
+        from("turb").
+        to_string(),
+
+      "DELETE col1, col2, col3 FROM turb "
+    );
   }
 
   template <> template <>
   void casst::delete_group::test<1>() /* where */
   {
-    expect_equal(casst::delete_().from("turb").
-                                  where(casst::equal("alive", false)).to_string(),
-                 "DELETE FROM turb WHERE alive = false");
-    expect_equal(casst::delete_().cols("col1", "col2", "col3").from("turb").
-                                  where(casst::equal("age", 18)).to_string(),
-                 "DELETE col1, col2, col3 FROM turb WHERE age = 18");
-    expect_equal(casst::delete_().cols("col1", "col2", "col3").from("turb").
-                                  where(casst::equal("name", "meow")).to_string(),
-                 "DELETE col1, col2, col3 FROM turb WHERE name = 'meow'");
-    expect_equal(casst::delete_().from("turb").
-                                  where(casst::row("meow").in("kitty", "cat")).
-                                  to_string(),
-                 "DELETE FROM turb WHERE meow IN ( 'kitty', 'cat' ) ");
+    expect_equal
+    (
+      casst::delete_().from("turb").
+        where(casst::equal("alive", false)).
+        to_string(),
+
+      "DELETE FROM turb WHERE alive = false"
+    );
+    expect_equal
+    (
+      casst::delete_().cols("col1", "col2", "col3").from("turb").
+        where(casst::equal("age", 18)).
+        to_string(),
+
+      "DELETE col1, col2, col3 FROM turb WHERE age = 18"
+    );
+    expect_equal
+    (
+      casst::delete_().cols("col1", "col2", "col3").from("turb").
+        where(casst::equal("name", "meow")).
+        to_string(),
+
+      "DELETE col1, col2, col3 FROM turb WHERE name = 'meow'"
+    );
+    expect_equal
+    (
+      casst::delete_().from("turb").
+        where(casst::row("meow").in("kitty", "cat")).
+        to_string(),
+
+      "DELETE FROM turb WHERE meow IN ( 'kitty', 'cat' ) "
+    );
   }
 
   template <> template <>
   void casst::delete_group::test<2>() /* where + and */
   {
-    expect_equal(casst::delete_().from("people").
-                                  where(casst::row("meow").in("kitty", "cat")).
-                                  if_
-                                  (
-                                    casst::equal("name", "meow"),
-                                    casst::and_(),
-                                    casst::equal("alive", false)
-                                  ).to_string(),
-                 "DELETE FROM people WHERE meow IN ( 'kitty', 'cat' ) "
-                 "IF name = 'meow' AND alive = false ");
+    expect_equal
+    (
+      casst::delete_().from("people").
+        where(casst::row("meow").in("kitty", "cat")).
+        if_
+        (
+          casst::equal("name", "meow"),
+          casst::and_(),
+          casst::equal("alive", false)
+        ).to_string(),
+
+      "DELETE FROM people WHERE meow IN ( 'kitty', 'cat' ) "
+      "IF name = 'meow' AND alive = false "
+    );
   }
 
   template <> template <>
   void casst::delete_group::test<3>() /* timestamp */
   {
-    expect_equal(casst::delete_().from("people").using_timestamp(476500).
-                                  where(casst::equal("alive", false)).to_string(),
-                 "DELETE FROM people USING TIMESTAMP 476500 WHERE alive = false");
-    expect_equal(casst::delete_().from("people").using_timestamp(0).
-                                  where(casst::equal("alive", false)).to_string(),
-                 "DELETE FROM people USING TIMESTAMP 0 WHERE alive = false");
+    expect_equal
+    (
+      casst::delete_().from("people").using_timestamp(476500).
+        where(casst::equal("alive", false)).
+        to_string(),
+
+      "DELETE FROM people USING TIMESTAMP 476500 WHERE alive = false"
+    );
+    expect_equal
+    (
+      casst::delete_().from("people").using_timestamp(0).
+        where(casst::equal("alive", false)).
+        to_string(),
+
+      "DELETE FROM people USING TIMESTAMP 0 WHERE alive = false"
+    );
   }
 
   template <> template <>
   void casst::delete_group::test<4>() /* exists */
   {
-    expect_equal(casst::delete_().from("turb").if_exists().to_string(),
-                 "DELETE FROM turb IF EXISTS ");
-    expect_equal(casst::delete_().cols().from("turb").if_exists().to_string(),
-                 "DELETE FROM turb IF EXISTS ");
-    expect_equal(casst::delete_().cols("col1", "col2", "col3").from("turb").
-                                  if_exists().to_string(),
-                 "DELETE col1, col2, col3 FROM turb IF EXISTS ");
+    expect_equal
+    (
+      casst::delete_().from("turb").if_exists().to_string(),
+
+      "DELETE FROM turb IF EXISTS "
+    );
+    expect_equal
+    (
+      casst::delete_().cols().from("turb").if_exists().to_string(),
+
+      "DELETE FROM turb IF EXISTS "
+    );
+    expect_equal
+    (
+      casst::delete_().cols("col1", "col2", "col3").from("turb").
+        if_exists().
+        to_string(),
+
+      "DELETE col1, col2, col3 FROM turb IF EXISTS "
+    );
   }
 
   template <> template <>
   void casst::delete_group::test<5>() /* row_slice */
   {
-    expect_equal(casst::delete_().from("people").
-                                  where(casst::row("meow").in("kitty", "cat")).
-                                  if_
-                                  (
-                                    casst::equal
-                                    (
-                                      casst::row_slice
-                                      (
-                                        casst::column("name"),
-                                        casst::column("age")
-                                      ),
-                                      casst::row_slice("meow", 18)
-                                    )
-                                  ).to_string(),
-                 "DELETE FROM people WHERE meow IN ( 'kitty', 'cat' ) "
-                 "IF ( name, age ) = ( 'meow', 18 ) ");
+    expect_equal
+    (
+      casst::delete_().from("people").
+        where(casst::row("meow").in("kitty", "cat")).
+        if_
+        (
+          casst::equal
+          (
+            casst::row_slice
+            (
+              casst::column("name"),
+              casst::column("age")
+            ),
+            casst::row_slice("meow", 18)
+          )
+        ).to_string(),
+
+      "DELETE FROM people WHERE meow IN ( 'kitty', 'cat' ) "
+      "IF ( name, age ) = ( 'meow', 18 ) "
+    );
   }
 }
